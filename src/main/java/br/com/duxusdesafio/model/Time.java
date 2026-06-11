83097dbe4ecf7c5e25a1b278231aec1c5d100ac4
Wallace <wmvwallace@gmail.com>
2026-06-11 06:47:00 -0300

    feat(model): implementar entidades JPA do domínio
    Adiciona modelo Integrante com validações @NotBlank e relacionamento com times.
    Adiciona modelo Time com clube, data única e composição de integrantes.
    Adiciona entidade associativa ComposicaoTime com chaves estrangeiras para Time e Integrante.

--- src/main/java/br/com/duxusdesafio/model/Time.java
+++ src/main/java/br/com/duxusdesafio/model/Time.java
@@ -1,17 +1,25 @@
 package br.com.duxusdesafio.model;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "time")
 public class Time {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     /**
      * Nome do Clube (associação esportiva)
@@ -20,38 +28,40 @@ public class Time {
      * Para Futebol - Palmeiras, Santos, etc;
      * Para Basquete - Pinheiros, Franca, etc.
      */
-    @Column
+    @NotBlank
+    @Column(nullable = false)
     private String nomeDoClube;
 
     /**
      * Data em que esse time foi formado (composição do time firmada)
      * Lembrando: a formação da equipe pode mudar em momentos diferentes, por isso a data.
      */
-	@Column
+    @NotNull
+    @Column(nullable = false, unique = true)
     private LocalDate data;
 
     /**
      * Elenco ou equipe - o conjunto de integrantes desse time
      */
-	@OneToMany(mappedBy = "time", cascade = CascadeType.ALL)
-	private List<ComposicaoTime> composicaoTime;
+    @OneToMany(mappedBy = "time", cascade = CascadeType.ALL)
+    private List<ComposicaoTime> composicaoTime;
 
-	public Time() {
-	}
+    public Time() {
+    }
 
-	public Time(String nomeDoClube, LocalDate data, List<ComposicaoTime> composicaoTime) {
-		this.nomeDoClube = nomeDoClube;
+    public Time(String nomeDoClube, LocalDate data, List<ComposicaoTime> composicaoTime) {
+        this.nomeDoClube = nomeDoClube;
         this.data = data;
-		this.composicaoTime = composicaoTime;
-	}
+        this.composicaoTime = composicaoTime;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getNomeDoClube() {
         return nomeDoClube;
@@ -62,41 +72,44 @@ public class Time {
     }
 
     public LocalDate getData() {
-		return data;
-	}
+        return data;
+    }
 
-	public void setData(LocalDate data) {
-		this.data = data;
-	}
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 
-	public List<ComposicaoTime> getComposicaoTime() {
-		return composicaoTime;
-	}
+    public List<ComposicaoTime> getComposicaoTime() {
+        return composicaoTime;
+    }
 
-	public void setComposicaoTime(List<ComposicaoTime> composicaoTime) {
-		this.composicaoTime = composicaoTime;
-	}
+    public void setComposicaoTime(List<ComposicaoTime> composicaoTime) {
+        this.composicaoTime = composicaoTime;
+    }
 
+    @Override
+    public final boolean equals(Object objeto) {
+        if (this == objeto) {
+            return true;
+        }
+        if (!(objeto instanceof Time)) {
+            return false;
+        }
+        Time outro = (Time) objeto;
+        return id != 0 && id == outro.id;
+    }
 
-	@Override
-	public final boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof Time)) return false;
-		Time time = (Time) o;
-		return id == time.id && Objects.equals(nomeDoClube, time.nomeDoClube) && Objects.equals(data, time.data);
-	}
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
+    }
 
-	@Override
-	public final int hashCode() {
-		return Objects.hash(id, nomeDoClube, data);
-	}
-
-	@Override
-	public String toString() {
-		return "Time{" +
-				"id=" + id +
-                ", nome=" + nomeDoClube +
-				", data=" + data +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Time{"
+                + "id=" + id
+                + ", nomeDoClube='" + nomeDoClube + '\''
+                + ", data=" + data
+                + '}';
+    }
 }
