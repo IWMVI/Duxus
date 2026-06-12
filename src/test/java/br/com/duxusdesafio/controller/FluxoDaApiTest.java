1f876f0a9c2d0dde9d903e950ceb7eb925efb7e9
Wallace <wmvwallace@gmail.com>
2026-06-12 12:31:29 -0300

    feat(database): configurar SQL Server como banco principal

--- src/test/java/br/com/duxusdesafio/controller/FluxoDaApiTest.java
+++ src/test/java/br/com/duxusdesafio/controller/FluxoDaApiTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class FluxoDaApiTest {
 
     @Autowired
