df70a87a64e0143ff57b5a82c727915a59ba6d4b
Wallace <wmvwallace@gmail.com>
2026-06-12 14:17:42 -0300

    style: padronizar indentação de tabs para 4 espaços
    - Converte tabs para 4 espaços em arquivos que ainda usavam tabs
    - Mantém consistência de formatação com o restante do projeto

--- src/main/java/br/com/duxusdesafio/DuxusdesafioApplication.java
+++ src/main/java/br/com/duxusdesafio/DuxusdesafioApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DuxusdesafioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DuxusdesafioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DuxusdesafioApplication.class, args);
+    }
 
 }
--- src/test/java/br/com/duxusdesafio/DuxusdesafioApplicationTests.java
+++ src/test/java/br/com/duxusdesafio/DuxusdesafioApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class DuxusdesafioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
